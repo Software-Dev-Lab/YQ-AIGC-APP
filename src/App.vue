06f235f334cf52f18a89b40bbf4e3c24db45ff4d
ZRMYDYCG <547471919@qq.com>
2024-08-15 22:53:59 +0800

    feat: 创建主页面的公共样式

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
+<!--
+ * @Author: ZRMYDYCG 547471919@qq.com
+ * @Date: 2024-08-15 20:07:12
+ * @LastEditors: ZRMYDYCG 547471919@qq.com
+ * @LastEditTime: 2024-08-15 22:42:13
+ * @Description: App.vue
+-->
 <script setup lang="ts">
 import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
 onLaunch(() => {
-  console.log("App Launch");
+  console.log("App Launch 🚀");
 });
 onShow(() => {
-  console.log("App Show");
+  console.log("App Show 🎉");
 });
 onHide(() => {
-  console.log("App Hide");
+  console.log("App Hide 😢");
 });
 </script>
-<style></style>
+<style>
+/* 每个页面的公共样式 */
+view, text {
+  display: block;
+  font-size: 33rpx;
+}
+image {
+  display: block;
+}
+button::after {
+  border: none;
+}
+button {
+  padding: 0;
+  margin: 0;
+  line-height: 0;
+  background-color: #fff;
+}
+</style>
---
+++ src/pages/chat-view/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <view class="chat-view">
+    <view class="chat-header">12</view>
+    <view class="chat-body">34</view>
+    <view class="chat-footer">56</view>
+  </view>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style scoped></style>
