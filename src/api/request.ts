14ae42f1e48c0e31919457f0bb0c0b6c775453fc
ZRMYDYCG <547471919@qq.com>
2024-09-01 17:49:18 +0800

    feat: 封装http请求

---
+++ src/api/request.ts
@@ -0,0 +1,58 @@
+/*
+ * @Author: ZRMYDYCG
+ * @Date: 2024-09
+ * @LastEditors: ZRMYDYCG
+ * @LastEditTime: 2024-09
+ * @Description: 网络请求
+ */
+const baseURL = 'http://192.168.80.45:7000'
+/**
+ * 添加拦截器:
+ *   拦截 request 请求
+ *   拦截 uploadFile 文件上传
+ *
+ * TODO:
+ *   1. 非 http 开头需拼接地址
+ *   2. 请求超时
+ *   3. 添加小程序端请求头标识
+ *   4. 添加 token 请求头标识
+ */
+
+/**
+ * 请求函数
+ * @param  UniApp.RequestOptions
+ * @returns Promise
+ *  1. 返回 Promise 对象
+ *  2. 获取数据成功
+ *    2.1 提取核心数据 res.data
+ *    2.2 添加类型，支持泛型
+ *  3. 获取数据失败
+ *    3.1 401错误  -> 清理用户信息，跳转到登录页
+ *    3.2 其他错误 -> 根据后端错误信息轻提示
+ *    3.3 网络错误 -> 提示用户换网络
+ */
+type Data<T> = {
+  code: string
+  msg: string
+  result: T
+}
+// 2.2 添加类型，支持泛型
+export const http = <T>(options: UniApp.RequestOptions) => {
+  // 1. 返回 Promise 对象
+  return new Promise<Data<T>>((resolve, reject) => {
+    uni.request({
+      ...options,
+      // 响应成功
+      success(res) {
+        // 状态码 2xx
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          // 2.1 提取核心数据 res.data
+          resolve(res.data as Data<T>)
+        }
+      },
+      // 响应失败
+      fail(err) {
+      },
+    })
+  })
+}
--- src/store/index.ts
+++ src/store/index.ts
@@ -48,5 +48,8 @@ export const useChatbotMessage = defineStore('chatbotMessageStore', {
             web_search: []
         })
        }
+       /**
+        * 
+       */
     }
 })
